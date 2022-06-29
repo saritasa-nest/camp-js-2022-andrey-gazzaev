82f03e9f677704426457f3fd7bf703f7c39f7dd3
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-06-29 18:00:19 +0700

    Layout sort element #JC19-329

--- apps/vanilla/src/pages/index.ts
+++ apps/vanilla/src/pages/index.ts
@@ -1,9 +1,21 @@
+import { DEFAULT_SORT_SETTINGS, LOCAL_STORAGE_SETTINGS } from '../constants/anime';
 import { changeAnimeData } from '../scipts/public';
+
+/**
+ * Add sorting settings to local storage if they are not there.
+ */
+const initSortSettings = (): void => {
+  if (localStorage.getItem(LOCAL_STORAGE_SETTINGS)) {
+    localStorage.setItem(LOCAL_STORAGE_SETTINGS, JSON.stringify(DEFAULT_SORT_SETTINGS));
+  }
+};
 
 /**
  * Table initialization and pagination.
  */
 const initTable = (): Promise<void> => {
+  initSortSettings();
+
   const firstPage = 1;
   return changeAnimeData(firstPage);
 };
