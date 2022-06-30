057192ec95a0975141feed98c7ec46ed7582c8d5
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-06-30 15:22:09 +0700

    Add anime model, dto, mapper #JC19-329

--- apps/vanilla/src/scripts/table.ts
+++ apps/vanilla/src/scripts/table.ts
@@ -1,53 +1,55 @@
+import { Anime } from '@js-camp/core/models/anime';
+
 import { AnimeSwitchCase, ANIME_OBJECT, NO_DATA } from '../constants/anime';
 import { C_TABLE } from '../constants/classes';
 import { T_IMG, T_TD, T_TR } from '../constants/tags';
-import { IAnime } from '../types/anime';
 
 /**
  * Creating and populating table rows.
  * @param animes List of anime entries.
  * @returns Array of rows.
  */
-const createTableRows = (animes: IAnime[]): HTMLTableRowElement[] => animes.map(anime => {
+const createTableRows = (animes: Anime[]): HTMLTableRowElement[] => animes.map(anime => {
   const row = document.createElement(T_TR);
 
-    for (const key in ANIME_OBJECT) {
-      const thElement = document.createElement(T_TD);
-      const imageElement = document.createElement(T_IMG);
-      const titleKey = String(ANIME_OBJECT[key]);
-      let aired = NO_DATA;
+  for (const key in ANIME_OBJECT) {
+    const thElement = document.createElement(T_TD);
+    const imageElement = document.createElement(T_IMG);
+    let aired = NO_DATA;
 
-      switch (key) {
-        case AnimeSwitchCase.Image:
-          // Adding an image to a table.
-          imageElement.src = anime[key];
-          thElement.append(imageElement);
-          row.append(thElement);
-          break;
+    switch (key) {
+      case AnimeSwitchCase.Image:
+        // Adding an image to a table.
+        imageElement.src = anime[key];
+        thElement.append(imageElement);
+        row.append(thElement);
+        break;
 
-        case AnimeSwitchCase.Aired:
-          // Adding an entry for aired start.
-          if (anime[AnimeSwitchCase.Aired].start) {
-            aired = new Date(anime[AnimeSwitchCase.Aired].start).toUTCString();
-          }
-          thElement.innerHTML = aired;
-          row.append(thElement);
-          break;
+      case AnimeSwitchCase.Aired:
+        // Adding an entry for aired start.
+        if (anime[AnimeSwitchCase.Aired].start) {
+          aired = new Date(anime[AnimeSwitchCase.Aired].start).toUTCString();
+        }
+        thElement.innerHTML = aired;
+        row.append(thElement);
+        break;
 
-        case AnimeSwitchCase.TitleEng:
-        case AnimeSwitchCase.TitleJpn:
-          // Add title in English and Japanese.
-          thElement.innerHTML = `${anime[titleKey] || NO_DATA}`;
-          row.append(thElement);
-          break;
+      case AnimeSwitchCase.TitleEng:
+      case AnimeSwitchCase.TitleJpn:
+      case AnimeSwitchCase.Type:
+      case AnimeSwitchCase.Status:
+        // Add title in English and Japanese, Type, Status.
+        thElement.innerHTML = `${anime[key] || NO_DATA}`;
+        row.append(thElement);
+        break;
 
-        default:
-          // Adding all other fields.
-          thElement.innerHTML = `${anime[key] || NO_DATA}`;
-          row.append(thElement);
-          break;
-      }
+      default:
+        // Adding all other fields.
+        // thElement.innerHTML = `${anime[key] || NO_DATA}`;
+        // row.append(thElement);
+        break;
     }
+  }
   return row;
 });
 
@@ -76,7 +78,7 @@ const updateTableAnime = (tableRows: HTMLTableRowElement[]): void => {
  * Fill the table with information about anime.
  * @param animes List of anime entries.
  */
-export const fillTableAnime = (animes: IAnime[]): void => {
+export const fillTableAnime = (animes: Anime[]): void => {
   const tableRows = createTableRows(animes);
 
   return updateTableAnime(tableRows);
---
+++ libs/core/mappers/anime.mapper.ts
@@ -0,0 +1,37 @@
+import { IAnimeResponseDTO } from '../dtos/anime.dto';
+import { Aired, Anime, AnimeResponse } from '../models/anime';
+
+export namespace AnimeMapper {
+
+  /**
+   * Maps dto to model.
+   * @param dto Anime dto.
+   */
+  export function fromDto(dto: IAnimeResponseDTO): AnimeResponse {
+    const results = dto.results.map(animeDto => {
+      const aired = new Aired({
+        end: animeDto.aired.end,
+        start: animeDto.aired.start,
+      });
+
+      return new Anime({
+        aired,
+        created: animeDto.created,
+        id: animeDto.id,
+        image: animeDto.image,
+        modified: animeDto.modified,
+        status: animeDto.status,
+        titleEng: animeDto.title_eng,
+        titleJpn: animeDto.title_jpn,
+        type: animeDto.type,
+      });
+    });
+
+    return new AnimeResponse({
+      count: dto.count,
+      next: dto.next,
+      previous: dto.previous,
+      results,
+    });
+  }
+}
