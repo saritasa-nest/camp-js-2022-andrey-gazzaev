01634d2e441d0d34937fc295e9e0d3408e80ae4f
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-04 16:17:50 +0700

    Fix "fetchAnime" #JC19-329

--- apps/vanilla/src/fetches/anime.ts
+++ apps/vanilla/src/fetches/anime.ts
@@ -14,14 +14,14 @@ import { FetchHeaders } from '../constants/fetch';
  */
 export async function fetchAnime(url: string): Promise<Pagination<Anime>> {
   try {
-    const instance = axios.get<PaginationDto<AnimeDto>>(url, {
+    const instance = await axios.get<PaginationDto<AnimeDto>>(url, {
       baseURL: ENV.baseUrl,
       headers: {
         [FetchHeaders.ApiKey]: ENV.apiKey,
       },
     });
 
-    const animeResponseDto = (await instance).data;
+    const animeResponseDto = instance.data;
 
     return AnimeMapper.fromDto(animeResponseDto);
   } catch {
