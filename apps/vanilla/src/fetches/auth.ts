ad16f561189c71ecb1d1c872291d148fa7714960
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-05 15:50:04 +0700

    Added tokens requests #JC19-338

--- apps/vanilla/src/fetches/auth.ts
+++ apps/vanilla/src/fetches/auth.ts
@@ -7,6 +7,9 @@ import { Tokens } from '@js-camp/core/models/tokens';
 import { HttpErrorMapper } from '@js-camp/core/mappers/httpError.mapper';
 import { HttpError } from '@js-camp/core/models/httpError';
 
+import { LOCAL_TOKENS } from '../constants/public';
+import { getLocalStorage, setLocalStorage } from '../scripts/localStorage';
+
 import { instance } from './instance';
 
 /**
@@ -19,9 +22,9 @@ export async function loginUser(
   password: string | null,
 ): Promise<Tokens | HttpError | Error> {
   try {
-    const LOGIN_URL = 'auth/login/';
+    const URL_LOGIN = 'auth/login/';
 
-    const response = await instance.post<TokensDto>(LOGIN_URL, {
+    const response = await instance.post<TokensDto>(URL_LOGIN, {
       email,
       password,
     });
@@ -39,5 +42,47 @@ export async function loginUser(
 
     const UNKNOWN_ERROR = 'unexpected error';
     throw new Error(UNKNOWN_ERROR);
+  }
+}
+
+/** Check if the token is valid. */
+export async function isVerifyToken(): Promise<boolean> {
+  try {
+    const tokens = getLocalStorage<Tokens>(LOCAL_TOKENS);
+    if (tokens === null) {
+      return false;
+    }
+
+    const URL_VERIFY_TOKEN = 'auth/token/verify';
+    await instance.post<TokensDto>(URL_VERIFY_TOKEN, {
+      token: tokens.access,
+    });
+
+    return true;
+  } catch (error: unknown) {
+
+    return false;
+  }
+}
+
+/** Refresh tokens. */
+export async function refreshToken(): Promise<boolean> {
+  try {
+    const tokens = getLocalStorage<Tokens>(LOCAL_TOKENS);
+    if (tokens === null) {
+      return false;
+    }
+
+    const URL_REFRESH_TOKEN = 'auth/token/verify';
+    const response = await instance.post<TokensDto>(URL_REFRESH_TOKEN, {
+      refresh: tokens.refresh,
+    });
+
+    setLocalStorage(LOCAL_TOKENS, TokensMapper.fromDto(response.data));
+
+    return true;
+  } catch (error: unknown) {
+
+    return false;
   }
 }
