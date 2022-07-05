4f48f19bf3eaa06ac9a1113d85d8bc5b2fecaa7e
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-05 09:04:54 +0700

    Fix fetches #JC19-329

--- apps/vanilla/src/fetches/instance.ts
+++ apps/vanilla/src/fetches/instance.ts
@@ -3,13 +3,11 @@ import axios, { AxiosInstance } from 'axios';
 import { FetchHeaders } from '../constants/fetch';
 
 /**
- * Create axios a request template.
+ * Axios request template.
  */
-export function getInstance(): AxiosInstance {
-  return axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-      [FetchHeaders.ApiKey]: import.meta.env.VITE_API_KEY,
-    },
-  });
-}
+export const instance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    [FetchHeaders.ApiKey]: import.meta.env.VITE_API_KEY,
+  },
+});
