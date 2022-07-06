1e8c5bd6629a5b481bdbbc11c2c3f4b20497e886
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-06 10:46:35 +0700

    Fix html #JC19-329

--- apps/vanilla/src/constants/classes.ts
+++ apps/vanilla/src/constants/classes.ts
@@ -26,9 +26,9 @@ export namespace Header {
 
 export namespace Catalog {
   export const BUTTON_PAGINATION = 'catalog__pagination-btn';
-  export const SELECT_SORT_STATUS = 'catalog__sort-status';
-  export const SELECT_SORT_DIRECTION = 'catalog__sort-direction';
-  export const SELECT_SORT_ORDERING = 'catalog__sort-ordering';
+  export const SELECT_SORT_STATUS = 'sort-status';
+  export const SELECT_SORT_DIRECTION = 'sort-direction';
+  export const SELECT_SORT_ORDERING = 'sort-ordering';
   export const PAGINATION = 'catalog__pagination';
   export const PAGINATION_BUTTON_CURRENT = 'catalog__pagination-btn_current';
   export const TABLE = 'catalog__table';
