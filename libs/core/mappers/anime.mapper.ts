b7948fc720120c7037b7b770793a1684d2619cda
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-06 10:43:43 +0700

    Added pagination mapper #JC19-329

--- libs/core/mappers/anime.mapper.ts
+++ libs/core/mappers/anime.mapper.ts
@@ -1,7 +1,5 @@
 import { AnimeDto } from '../dtos/anime.dto';
-import { PaginationDto } from '../dtos/pagination.dto';
 import { Anime, DateRange, Status, Type } from '../models/anime';
-import { Pagination } from '../models/pagination';
 
 export namespace AnimeMapper {
 
@@ -9,32 +7,25 @@ export namespace AnimeMapper {
    * Maps dto to model.
    * @param dto Anime dto.
    */
-  export function fromDto(dto: PaginationDto<AnimeDto>): Pagination<Anime> {
-    const results = dto.results.map(animeDto => {
-      const aired = new DateRange({
-        end: new Date(animeDto.aired.end),
-        start: new Date(animeDto.aired.start),
-      });
+  export function fromDto(dto: AnimeDto): Anime {
 
-      const status = animeDto.status in Status ? animeDto.status as Status : Status.Airing;
-      const type = animeDto.type in Type ? animeDto.type as Type : Type.Tv;
-
-      return new Anime({
-        id: animeDto.id,
-        image: animeDto.image,
-        titleEnglish: animeDto.title_eng,
-        titleJapanese: animeDto.title_jpn,
-        aired,
-        status,
-        type,
-      });
+    const aired = new DateRange({
+      end: new Date(dto.aired.end),
+      start: new Date(dto.aired.start),
     });
 
-    return new Pagination<Anime>({
-      count: dto.count,
-      next: dto.next,
-      previous: dto.previous,
-      results,
+    const status = dto.status in Status ? dto.status as Status : Status.Airing;
+    const type = dto.type in Type ? dto.type as Type : Type.Tv;
+
+    return new Anime({
+      id: dto.id,
+      image: dto.image,
+      titleEnglish: dto.title_eng,
+      titleJapanese: dto.title_jpn,
+      aired,
+      status,
+      type,
     });
+
   }
 }
