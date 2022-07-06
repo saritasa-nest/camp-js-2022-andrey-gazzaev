b01be8320c82304072d94e9568b91894c7acd469
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-06 10:43:43 +0700

    Added pagination mapper #JC19-329

--- apps/vanilla/src/fetches/anime.ts
+++ apps/vanilla/src/fetches/anime.ts
@@ -1,6 +1,7 @@
 import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
+import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 import { Anime } from '@js-camp/core/models/anime';
 import { HttpError } from '@js-camp/core/models/httpError';
 import { Pagination } from '@js-camp/core/models/pagination';
@@ -15,9 +16,7 @@ export async function fetchAnime(url: string): Promise<Pagination<Anime> | HttpE
   try {
     const response = await instance.get<PaginationDto<AnimeDto>>(url);
 
-    const animePaginationDto = response.data;
-
-    return AnimeMapper.fromDto(animePaginationDto);
+    return PaginationMapper.fromDto<AnimeDto, Anime>(response.data, AnimeMapper.fromDto);
   } catch (error: unknown) {
 
     if (error instanceof HttpError) {
