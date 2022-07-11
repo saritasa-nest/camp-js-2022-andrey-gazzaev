6bb93945a8855fbf6188617190cbba066ad1a7ac
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-11 12:40:55 +0700

    Add core elements #JC19-326

---
+++ libs/core/guards/error.guard.ts
@@ -0,0 +1,9 @@
+import { HttpErrorDto } from '../dtos/httpError.dto';
+
+/**
+ * Check if the error is of type HttpErrorDto.
+ * @param error Some error.
+ */
+export function isHttpErrorDto(error: unknown): error is HttpErrorDto {
+  return (error as HttpErrorDto).detail !== undefined;
+}
