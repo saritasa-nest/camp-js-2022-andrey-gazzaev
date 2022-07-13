efc8dac6ab60a7564c2ab34196264868d61e6a2d
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-13 10:50:31 +0700

    Refactor code #JC19-347

--- apps/vanilla/src/UI/details/details.ts
+++ apps/vanilla/src/UI/details/details.ts
@@ -1,4 +1,7 @@
 import { Anime } from '@js-camp/core/models/anime';
+import { DateRange } from '@js-camp/core/models/dateRange';
+import { Genre } from '@js-camp/core/models/genre';
+import { Studio } from '@js-camp/core/models/studio.dto';
 
 import { getDetailsAnime } from '../../services/general';
 
@@ -16,67 +19,137 @@ function createListItem(itemText: string): HTMLLIElement {
 }
 
 /**
+ * Sets src to image.
+ * @param imageSrc Image URL.
+ */
+function setImage(imageSrc: string): void {
+  const imageElement = document.querySelector<HTMLImageElement>(`.details__image`);
+  if (imageElement !== null) {
+    imageElement.src = imageSrc;
+  }
+}
+
+/**
+ * Sets text to title element.
+ * @param title Title text.
+ */
+function setTitle(title: string): void {
+  const titleElement = document.querySelector(`.details__anime-title`);
+  if (titleElement !== null) {
+    titleElement.innerHTML = title;
+  }
+}
+
+/**
+ * Sets text to synopsis element.
+ * @param synopsis Synopsis text.
+ */
+function setSynopsis(synopsis: string): void {
+  const synopsisElement = document.querySelector(`.details__anime-synopsis`);
+  if (synopsisElement !== null) {
+    synopsisElement.innerHTML = synopsis;
+  }
+}
+
+/**
+ * Sets airing element.
+ * @param isAiring Airing option.
+ */
+function setAiring(isAiring: boolean): void {
+  const airingElement = document.querySelector(`.details__anime-airing`);
+  if (airingElement !== null) {
+    airingElement.innerHTML = `Airing: ${isAiring ? 'Yes' : 'No'}`;
+  }
+}
+
+/**
+ * Sets date range to aired element.
+ * @param aired Aired date range.
+ */
+function setAired(aired: DateRange): void {
+  const airedElement = document.querySelector(`.details__anime-aired`);
+  if (airedElement !== null) {
+    const airedStart = aired.start ?
+      aired.start.toLocaleDateString('en-GB', { timeZone: 'UTC' }) :
+      NO_DATA;
+
+    const airedEnd = aired.end ?
+      aired.end.toLocaleDateString('en-GB', { timeZone: 'UTC' }) :
+      NO_DATA;
+    airedElement.innerHTML = `Aired: from ${airedStart} to ${airedEnd}`;
+  }
+}
+
+/**
+ * Sets list studios to studios element.
+ * @param studios List studios.
+ */
+function setStudios(studios: readonly Studio[]): void {
+  const studiosElement = document.querySelector(`.details__list-studios`);
+  if (studiosElement !== null) {
+    const studiosItems = studios.map(studio => createListItem(studio.name));
+
+    studiosElement.append(...studiosItems);
+  }
+}
+
+/**
+ * Sets list genres to genres element.
+ * @param genres List genres.
+ */
+function setGenres(genres: readonly Genre[]): void {
+  const genresElement = document.querySelector(`.details__list-genres`);
+  if (genresElement !== null) {
+    const genresItems = genres.map(genre => createListItem(genre.name));
+
+    genresElement.append(...genresItems);
+  }
+}
+
+/**
+ * Sets src to video.
+ * @param trailerId Video youtube id.
+ */
+function setTrailer(trailerId: string): void {
+  const trailerElement = document.querySelector<HTMLVideoElement>(`.anime-trailer__video`);
+  if (trailerElement !== null) {
+    trailerElement.src = `http://www.youtube.com/embed/${trailerId}/`;
+  }
+}
+
+/**
  * Renders anime card.
  * @param anime Information about anime.
  */
 function renderAnimeCard(anime: Anime): void {
+  setImage(anime.image);
 
-  const imageElement = document.querySelector<HTMLImageElement>(`.details__image`);
-  if (imageElement !== null) {
-    imageElement.src = anime.image;
+  setTitle(`${anime.titleEnglish || NO_DATA} / ${anime.titleJapanese || NO_DATA}`);
+
+  setSynopsis(`${anime.synopsis ?? NO_DATA}`);
+
+  if (anime.airing !== undefined) {
+    setAiring(anime.airing);
   }
 
-  const titleElement = document.querySelector(`.details__anime-title`);
-  if (titleElement !== null) {
-    titleElement.innerHTML = `${anime.titleEnglish || NO_DATA} / ${anime.titleJapanese || NO_DATA}`;
+  setAired(anime.aired);
+
+  if (anime.studiosData !== undefined) {
+    setStudios(anime.studiosData);
   }
 
-  const synopsisElement = document.querySelector(`.details__anime-synopsis`);
-  if (synopsisElement !== null) {
-    synopsisElement.innerHTML = `${anime.synopsis ?? NO_DATA}`;
+  if (anime.genresData !== undefined) {
+    setGenres(anime.genresData);
   }
 
-  const airingElement = document.querySelector(`.details__anime-airing`);
-  if (airingElement !== null && anime.airing !== undefined) {
-    const airing = anime.airing ? 'Yes' : 'No';
-    airingElement.innerHTML = `Airing: ${airing}`;
-  }
-
-  const airedElement = document.querySelector(`.details__anime-aired`);
-  if (airedElement !== null) {
-    const airedStart = anime.aired.start ?
-      anime.aired.start.toLocaleDateString('en-GB', { timeZone: 'UTC' }) :
-      NO_DATA;
-
-    const airedEnd = anime.aired.end ?
-      anime.aired.end.toLocaleDateString('en-GB', { timeZone: 'UTC' }) :
-      NO_DATA;
-    airedElement.innerHTML = `Aired: from ${airedStart} to ${airedEnd}`;
-  }
-
-  const studiosElement = document.querySelector(`.details__list-studios`);
-  if (studiosElement !== null && anime.studiosData !== undefined) {
-    const studiosItems = anime.studiosData.map(studio => createListItem(studio.name));
-
-    studiosElement.append(...studiosItems);
-  }
-
-  const genresElement = document.querySelector(`.details__list-genres`);
-  if (genresElement !== null && anime.genresData !== undefined) {
-    const genresItems = anime.genresData.map(studio => createListItem(studio.name));
-
-    genresElement.append(...genresItems);
-  }
-
-  const trailerElement = document.querySelector<HTMLVideoElement>(`.anime-trailer__video`);
-  if (trailerElement !== null && anime.trailerYoutubeId !== undefined) {
-    trailerElement.src = `https://www.youtube.com/embed/${anime.trailerYoutubeId}`;
+  if (anime.trailerYoutubeId !== undefined) {
+    setTrailer(anime.trailerYoutubeId);
   }
 }
 
 /** Renders details anime. */
 export async function renderDetailsAnime(): Promise<void> {
-  const testAnimeId = 1;
+  const testAnimeId = 37;
   const anime = await getDetailsAnime(testAnimeId);
 
   if (anime instanceof Anime) {
