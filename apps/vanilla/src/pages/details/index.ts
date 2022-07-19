6558bc62d9289ac2594801edc96a4f5bd437e80b
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-19 16:54:08 +0700

    Add back link #JC19-347

--- apps/vanilla/src/pages/details/index.ts
+++ apps/vanilla/src/pages/details/index.ts
@@ -1,9 +1,15 @@
 import { renderDetailsAnime } from '../../UI/details/details';
+import { getDomElement } from '../../UI/general';
 import { renderHeader } from '../../UI/header/header';
+
+const BACK_LINK_CLASS = 'back-link';
 
 /** Initializations page. */
 function initPage(): void {
   renderHeader();
+
+  getDomElement(document, `.${BACK_LINK_CLASS}`).addEventListener('click', () => window.history.back());
+
   renderDetailsAnime();
 }
 
