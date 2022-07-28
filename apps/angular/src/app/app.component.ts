a3e4b46ab91cb35b929e899d49c1fc9d2b1d63b2
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-28 13:11:54 +0700

    Replace css #JC19-395

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: [],
+  styleUrls: [
+    '../theme/styles.css',
+    '../theme/css-reset.css',
+  ],
 })
 export class AppComponent {}
