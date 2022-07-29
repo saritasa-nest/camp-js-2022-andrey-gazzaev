933a1bed71ac1b90efbe0aec266aedca646bec69
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-07-29 12:48:18 +0700

    Dev login #JC19-427

--- apps/angular/src/app/features/auth/auth.module.ts
+++ apps/angular/src/app/features/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
@@ -8,6 +10,11 @@ import { RegistrationComponent } from './registration/registration.component';
 /** Authorization module. */
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule {}
+export class AuthModule { }
--- apps/angular/src/app/features/auth/login/login.component.ts
+++ apps/angular/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface LoginFormControls {
+
+  /** User email. */
+  readonly email: FormControl<string | null>;
+
+  /** User password. */
+  readonly password: FormControl<string | null>;
+}
+
+/** Login component. */
 @Component({
   selector: 'camp-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
 
-  ngOnInit(): void {}
+  /** Login form. */
+  public readonly loginForm: FormGroup<LoginFormControls>;
+
+  public constructor(
+    private readonly formBuilder: FormBuilder,
+  ) {
+    this.loginForm = this.initLoginForm();
+  }
+
+  private initLoginForm(): FormGroup<LoginFormControls> {
+    return this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    },
+    { updateOn: 'blur' });
+  }
 }
