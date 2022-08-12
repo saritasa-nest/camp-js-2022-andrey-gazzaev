eb8dad5b516db7ea31fcf6cbff07d39082e3014b
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-08-12 17:55:15 +0700

    Add trackBy Genre and Studio #JC19-437

--- apps/angular/src/app/features/anime/details/details.component.ts
+++ apps/angular/src/app/features/anime/details/details.component.ts
@@ -1,10 +1,12 @@
 import { map, Observable, switchMap } from 'rxjs';
 
-import { Component } from '@angular/core';
+import { Component, TrackByFunction } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
+import { Genre } from '@js-camp/core/models/genre';
+import { Studio } from '@js-camp/core/models/studio.dto';
 import { AnimeDetails } from '@js-camp/core/models/animeDetails';
 
 import { AnimeService } from '../../../../core/services/anime.service';
@@ -39,11 +41,11 @@ export class DetailsComponent {
 
     this.trailerUrl$ = this.anime$.pipe(
       map(anime => {
-         // Trailer plays via iframe.
-         // Unfortunately, the url of this player is not reliable.
-         // But if we want to use the player, we need to trust this url.
-         const trailerUrl = `https://www.youtube.com/embed/${anime.trailerYoutubeId}`;
-         return this.sanitizer.bypassSecurityTrustResourceUrl(trailerUrl);
+        // Trailer plays via iframe.
+        // Unfortunately, the url of this player is not reliable.
+        // But if we want to use the player, we need to trust this url.
+        const trailerUrl = `https://www.youtube.com/embed/${anime.trailerYoutubeId}`;
+        return this.sanitizer.bypassSecurityTrustResourceUrl(trailerUrl);
       }),
     );
   }
@@ -61,4 +63,22 @@ export class DetailsComponent {
       },
     });
   }
+
+  /**
+   * Tracks anime by ID.
+   * @param _index Anime's index into array.
+   * @param genre Object of genre.
+   */
+  public trackItemGenre: TrackByFunction<Genre> = function(_index: number, genre: Genre): number {
+    return genre.id;
+  };
+
+  /**
+   * Tracks anime by ID.
+   * @param _index Anime's index into array.
+   * @param studio Object of studio.
+   */
+  public trackItemStudio: TrackByFunction<Studio> = function(_index: number, studio: Studio): number {
+    return studio.id;
+  };
 }
