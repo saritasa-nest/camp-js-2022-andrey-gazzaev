45cb115a701d2f1aa2119bc2d0ab2eeba9c27796
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-08-19 18:08:13 +0700

    Add titles component #JC19-467

---
+++ apps/react/src/features/anime/components/AnimeItem/Titles/Titles.tsx
@@ -0,0 +1,34 @@
+import { FC, memo } from 'react';
+import { Typography } from '@mui/material';
+
+import { replaceNull } from '../../../../../features/auth/utils/text';
+
+interface Props {
+
+  /** Title of English. */
+  readonly titleEnglish: string | null;
+
+  /** Title of Japanese. */
+  readonly titleJapanese: string | null;
+}
+
+const AnimeTitlesComponent: FC<Props> = ({ titleEnglish, titleJapanese }) => (
+  <>
+    <Typography
+      component="p"
+      variant="subtitle1"
+      color="text.primary"
+    >
+      {replaceNull(titleEnglish)}
+    </Typography>
+    <Typography
+      component="p"
+      variant="subtitle2"
+      color="text.primary"
+    >
+      {replaceNull(titleJapanese)}
+    </Typography>
+  </>
+);
+
+export const Titles = memo(AnimeTitlesComponent);
