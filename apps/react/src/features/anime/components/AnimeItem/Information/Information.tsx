95438d1a83ed78bf451332227438264cc4436e44
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-08-19 18:08:43 +0700

    Add information component #JC19-467

---
+++ apps/react/src/features/anime/components/AnimeItem/Information/Information.tsx
@@ -0,0 +1,34 @@
+import { FC, memo } from 'react';
+import { Typography } from '@mui/material';
+
+import { replaceNull } from '../../../../../features/auth/utils/text';
+
+interface Props {
+
+  /** Title of English. */
+  readonly status: string | null;
+
+  /** Title of Japanese. */
+  readonly type: string | null;
+}
+
+const AnimeInformationComponent: FC<Props> = ({ status, type }) => (
+  <>
+    <Typography
+      component="p"
+      variant="body2"
+      color="text.primary"
+    >
+      Type: {replaceNull(type)}
+    </Typography>
+    <Typography
+      component="p"
+      variant="body2"
+      color="text.primary"
+    >
+      Status: {replaceNull(status)}
+    </Typography>
+  </>
+);
+
+export const Information = memo(AnimeInformationComponent);
