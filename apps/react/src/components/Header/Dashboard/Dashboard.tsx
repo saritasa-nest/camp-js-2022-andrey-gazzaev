9dc949dfbb02524924ae6d2048f36dfa31c57f97
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-08-22 16:12:03 +0700

    Add dashboard component into header #JC19-455

---
+++ apps/react/src/components/Header/Dashboard/Dashboard.tsx
@@ -0,0 +1,64 @@
+import { FC, memo, useState } from 'react';
+import { Box, Button, Menu, MenuItem, Typography } from '@mui/material';
+import { useAppDispatch, useAppSelector } from '@js-camp/react/store/store';
+import { logoutUser } from '@js-camp/react/store/user/dispatchers';
+import { selectUser } from '@js-camp/react/store/user/selector';
+
+const DashboardComponent: FC = () => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleUserLogout = () => {
+    dispatch(logoutUser());
+    setAnchorEl(null);
+  };
+
+  if (user === null) {
+    return <p>Oops, something went wrong.</p>;
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <Button
+        id="basic-button"
+        color='inherit'
+        variant="text"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem >
+          <Typography variant="h6" component="span">
+            {user.firstName} {user.lastName}
+          </Typography>
+
+        </MenuItem>
+        <MenuItem onClick={handleUserLogout}>Logout</MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
+export const Dashboard = memo(DashboardComponent);
--- apps/react/src/components/Header/Header.tsx
+++ apps/react/src/components/Header/Header.tsx
@@ -1,48 +1,19 @@
 import { FC, memo } from 'react';
-import { Button, Typography, Toolbar, Box, AppBar } from '@mui/material';
+import { Typography, Toolbar, Box, AppBar } from '@mui/material';
 
-import { selectUser } from '@js-camp/react/store/user/selector';
-import { logoutUser } from '@js-camp/react/store/user/dispatchers';
+import { Dashboard } from './Dashboard';
 
-import { useAppDispatch, useAppSelector } from '../../store';
-
-const HeaderComponent: FC = () => {
-  const user = useAppSelector(selectUser);
-  const dispatch = useAppDispatch();
-
-  const handleUserLogout = () => {
-    dispatch(logoutUser());
-  };
-
-  if (user === null) {
-    return <p>Oops, something went wrong.</p>;
-  }
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+const HeaderComponent: FC = () => (
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
             Anime Catalog
-          </Typography>
-
-          <Box>
-            <Typography variant="h6" component="span">
-              Hello {user.firstName} {user.lastName}
-            </Typography>
-
-            <Button
-              type="button"
-              color="inherit"
-              onClick={handleUserLogout}
-            >
-              Log Out
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
+        </Typography>
+        <Dashboard />
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
 
 export const Header = memo(HeaderComponent);
