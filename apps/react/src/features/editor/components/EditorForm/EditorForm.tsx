aa4acb2a56d4a9cfd86622898442d4787a282e7a
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2022-08-24 18:01:40 +0700

    Add simple select to form #JC19-497

--- apps/react/src/features/editor/components/EditorForm/EditorForm.tsx
+++ apps/react/src/features/editor/components/EditorForm/EditorForm.tsx
@@ -1,8 +1,16 @@
+import { AnimeStatus, AnimeType } from '@js-camp/core/models/anime';
+import { Season, Source } from '@js-camp/core/models/anime-editor';
 import { LoadingButton } from '@mui/lab';
-import { Box, Grid, Paper } from '@mui/material';
+import { Box, Grid, InputLabel, MenuItem, Paper, Rating, Select } from '@mui/material';
 import { Field, FormikProvider, useFormik } from 'formik';
 import { TextField } from 'formik-mui';
 import { memo } from 'react'
+
+const types = Object.values(AnimeType);
+const statuses = Object.values(AnimeStatus);
+const sources = Object.values(Source);
+const seasons = Object.values(Season);
+const ratings = Object.values(Rating);
 
 const EditorFormComponent = () => {
   const isLoading = false;
@@ -65,6 +73,72 @@ const EditorFormComponent = () => {
               required
             />
           </Grid>
+
+          <Grid item xs={12}>
+            <InputLabel id="types">Type</InputLabel>
+            <Field
+              component={Select}
+              id="types"
+              name="types"
+              label="Type"
+              required
+            >
+              {types.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
+            </Field>
+          </Grid>
+
+          <Grid item xs={12}>
+            <InputLabel id="statuses">Status</InputLabel>
+            <Field
+              component={Select}
+              id="statuses"
+              name="statuses"
+              label="Status"
+              required
+            >
+              {statuses.map(status => <MenuItem key={status} value={status}>{status}</MenuItem>)}
+            </Field>
+          </Grid>
+
+          <Grid item xs={12}>
+            <InputLabel id="ratings">Rating</InputLabel>
+            <Field
+              component={Select}
+              id="ratings"
+              name="ratings"
+              label="Rating"
+              required
+            >
+              {ratings.map(rating => <MenuItem key={rating} value={rating}>{rating}</MenuItem>)}
+            </Field>
+          </Grid>
+
+          <Grid item xs={12}>
+            <InputLabel id="sources">Source</InputLabel>
+            <Field
+              component={Select}
+              id="sources"
+              name="sources"
+              label="Source"
+              required
+            >
+              {sources.map(source => <MenuItem key={source} value={source}>{source}</MenuItem>)}
+            </Field>
+          </Grid>
+
+          <Grid item xs={12}>
+            <InputLabel id="seasons ">Seasons</InputLabel>
+            <Field
+              component={Select}
+              id="seasons"
+              name="seasons"
+              label="Season"
+              required
+            >
+              {seasons.map(season => <MenuItem key={season} value={season}>{season}</MenuItem>)}
+            </Field>
+          </Grid>
+
           <Grid item xs={12}>
             <LoadingButton
               loading={isLoading}
@@ -80,7 +154,7 @@ const EditorFormComponent = () => {
 
       </Paper>
     </FormikProvider >
-  )
-}
+  );
+};
 
 export const EditorForm = memo(EditorFormComponent);
